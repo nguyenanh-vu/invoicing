3478124f49ef82a3e72a7147809bc3d025d3f53e
NguyenAnhVUNancy <nguyenanhvu08@gmail.com>
2025-02-09 18:47:39 +0100

    09/02/2025: version 0.0.1

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup
+
+sys.path.append("./src")
+import __version__
+
+version = __version__.__version__
+
+setup(
+   name='invoicing',
+   version=version,
+   description='Module for automating invoicing creation',
+   license="GPLv3",
+   long_description="",
+   author='nguyenanh-vu',
+   author_email='nguyenanhvu08@gmail.com',
+   url="https://github.com/nguyenanh-vu/invoicing",
+   packages=['invoicing'],
+   package_dir = {'invoicing': 'src'},
+   data_files = [('config', ['data/conf/conf.ini.template'])],
+   install_requires=["google-auth-oauthlib",
+                     "google-auth-httplib2",
+                     "google-api-python-client"
+                     ]
+)
